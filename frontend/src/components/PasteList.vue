edb9739d26637cedad2866ee74a107242ae64e90
Ryan Stolliker <ryan.stolliker@gmail.com>
2024-08-05 01:30:55 -0700

    PasteList tailwind setup

--- frontend/src/components/PasteList.vue
+++ frontend/src/components/PasteList.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="paste in pastes" :key="paste.id">
-        {{ paste.content }}
-      </li>
-    </ul>
-    <div class="pagination">
-      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
-      <span>Page {{ currentPage }} of {{ totalPages }}</span>
-      <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+  <div class="container mx-auto px-4">
+    <div class="grid grid-cols-1 gap-6 mt-4">
+      <div
+        v-for="paste in pastes"
+        :key="paste.id"
+        class="bg-white shadow-md rounded-lg p-4 border-2 border-gray-200"
+      >
+        <div class="text-sm font-semibold text-gray-600 mb-2">ID: {{ paste.id }}</div>
+        <div class="text-gray-800 whitespace-pre-wrap">{{ paste.content }}</div>
+      </div>
+    </div>
+    <div class="mt-6 flex justify-center">
+      <button
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Previous
+      </button>
+      <span class="py-2 px-4">Page {{ currentPage }} of {{ totalPages }}</span>
+      <button
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2 disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Next
+      </button>
     </div>
   </div>
 </template>
